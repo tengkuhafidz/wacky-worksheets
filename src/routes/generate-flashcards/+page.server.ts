b60b7aefa66786b3509056e446595e6b7ac8301e
Shang Liang <shang@estl.sg>
2023-02-10 06:57:19 +0800

    Add concurrency to openai calls for flashcards generation

--- src/routes/generate-flashcards/+page.server.ts
+++ src/routes/generate-flashcards/+page.server.ts
@@ -57,23 +57,19 @@ const getTopicItems = async (topic: string) => {
 const generateImages = async (topicItems: string[], creativeScene: string, artStyle: string, drawingTool: string, colours: string) => {
     console.log("Starting to generate images with openai...", topicItems, creativeScene, artStyle, drawingTool, colours)
 
-    const responses = []
+    let base64Images
+    const requests = []
     for (const item of topicItems) {
-        try {
-            const response = await openai.createImage({
-                prompt: `${item} ${creativeScene}, ${artStyle} made with ${drawingTool}, ${colours} vibe`,
-                n: 1,
-                size: '256x256',
-                response_format: 'b64_json'
-            })
-            responses.push(response)
-
-        } catch (e) {
-            console.log("ERROR: generateImages", e)
-        }
+        requests.push(openai.createImage({
+            prompt: `${item} ${creativeScene}, ${artStyle} made with ${drawingTool}, ${colours} vibe`,
+            n: 1,
+            size: '256x256',
+            response_format: 'b64_json'
+        }))
     }
-
-    const base64Images = responses.map(response => response?.data?.data?.[0].b64_json)
+    await Promise.all(requests).then(responses =>
+        base64Images = responses.map(response => response?.data?.data?.[0].b64_json)
+    ).catch(e => console.log("Error: generateFlashcards", e))
 
     return {
         base64Images
